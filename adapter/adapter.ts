3c2ae8e59e4ff98d9e98e6813dfb1c152c274ebf
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-12-11 19:52:47 +0600

    feat: Adapter - Implements CNG Interface as BajajAuto

--- adapter/adapter.ts
+++ adapter/adapter.ts
@@ -8,9 +8,16 @@ interface CNGVehicle {
 
 // Fuel Vehicle Implementation
 class Toyota implements FuelVehicle {
-    refillFuel() {
-      console.log("Refilling octane in oil tank.....");
-    }
+  refillFuel() {
+    console.log("Refilling octane in oil tank.....");
   }
-  
+}
+
+// CNG Vehicle Implementation
+class BajajAutoRickshaw implements CNGVehicle {
+  refillCNG() {
+    console.log("Refilling CNG in gas tank.....");
+  }
+}
+
 
