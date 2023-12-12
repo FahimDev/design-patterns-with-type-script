3ff7624ce586738c2337458dbb1dfdefda7a277d
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-12-12 19:11:08 +0600

    feat: Decorator - Adds Three Concrete Option Classes

--- decorator/decorator.ts
+++ decorator/decorator.ts
@@ -9,7 +9,7 @@ abstract class Car {
 }
 
 abstract class CarOptions extends Car {
-    private decoratedCar!: Car;
+    decoratedCar!: Car;
     public abstract getDescription(): string;
 }
 
@@ -34,5 +34,52 @@ class ModelX extends Car {
 
     public cost(): number {
         return 60000;    
+    }
+}
+
+// ----- OPTIONS -----
+
+class AutoDrive extends CarOptions {
+    constructor(carInstance: Car) {
+        super();
+        this.decoratedCar = carInstance;
+    }
+
+    public getDescription(): string {
+        return this.decoratedCar.getDescription() + "Enhanced Auto Driving.";
+    }
+
+    public cost(): number {
+        return this.decoratedCar.cost() + 1000;
+    }
+}
+
+class AirSuspension extends CarOptions {
+    constructor(carInstance: Car) {
+        super();
+        this.decoratedCar = carInstance;
+    }
+
+    public getDescription(): string {
+        return this.decoratedCar.getDescription() + "Smart Air Suspension";
+    }
+
+    public cost(): number {
+        return this.decoratedCar.cost() + 800;
+    }
+}
+
+class SentryMode extends CarOptions {
+    constructor(carInstance: Car) {
+        super();
+        this.decoratedCar = carInstance;
+    }
+
+    public getDescription(): string {
+        return this.decoratedCar.getDescription() + "Sentry Mode Monitoring";
+    }
+
+    public cost(): number {
+        return this.decoratedCar.cost() + 300;
     }
 }
