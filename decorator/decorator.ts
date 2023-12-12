d204a475a94a1f61ad7e559a8d11d7904e6ca2c1
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-12-12 18:50:35 +0600

    feat: Decorator - Creates Two Concrete Car Classes

--- decorator/decorator.ts
+++ decorator/decorator.ts
@@ -12,3 +12,27 @@ abstract class CarOptions extends Car {
     private decoratedCar!: Car;
     public abstract getDescription(): string;
 }
+
+// ----- CAR -----
+
+class ModelS extends Car {
+    public description = "Model S";
+    public getDescription(): string {
+        return this.description;
+    }
+
+    public cost(): number {
+        return 70000;
+    }
+}
+
+class ModelX extends Car {
+    public description = "Model X";
+    public getDescription(): string {
+        return this.description;
+    } 
+
+    public cost(): number {
+        return 60000;    
+    }
+}
