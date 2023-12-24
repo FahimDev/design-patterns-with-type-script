6d8f1ab364eb39e9c626b2c46bfa35eccdce5469
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-12-24 16:01:07 +0600

    fear: Factory - Implements Concrete Classes of Automobile

--- factory/factory.ts
+++ factory/factory.ts
@@ -3,3 +3,24 @@ interface IAutomobile {
   stop(): void;
 }
 
+// Concrete class for Sedan
+class Sedan implements IAutomobile {
+  start(): void {
+    console.log("Sedan is starting....");
+  }
+
+  stop(): void {
+    console.log("Sedan is stopping.....");
+  }
+}
+
+// Concrete class for SUV
+class SUV implements IAutomobile {
+  start(): void {
+    console.log("SUV is starting....");
+  }
+  stop(): void {
+    console.log("SUV is stopping....");
+  }
+}
+
