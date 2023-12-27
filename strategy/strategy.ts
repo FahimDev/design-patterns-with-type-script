36580010e0c22e112393f86df941b879fd649f98
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-12-27 20:02:14 +0600

    feat: Strategy - Adds Execution Test

--- strategy/strategy.ts
+++ strategy/strategy.ts
@@ -6,21 +6,21 @@ interface CommunicationStrategy {
 // Concrete implementation of a communication strategy using MQTT protocol
 class MqttCommunicationStrategy implements CommunicationStrategy {
   send(data: any): void {
-    console.log(`Sending data via MQTT: ${data}`);
+    console.log(`Sending data via MQTT: ${JSON.stringify(data)}`);
   }
 }
 
 // Concrete implementation of a communication strategy using HTTP protocol
 class HttpCommunicationStrategy implements CommunicationStrategy {
   send(data: any): void {
-    console.log(`Sending data via HTTP: ${data}`);
+    console.log(`Sending data via HTTP: ${JSON.stringify(data)}`);
   }
 }
 
 // Concrete implementation of a communication strategy using WebSocket (WSS) protocol
 class WssCommunicationStrategy implements CommunicationStrategy {
   send(data: any): void {
-    console.log(`Sending data via WSS: ${data}`);
+    console.log(`Sending data via WSS: ${JSON.stringify(data)}`);
   }
 }
 
@@ -42,3 +42,30 @@ class IoTDevice {
     this.communicationStrategy.send(data);
   }
 }
+
+
+const payload: any = {
+    'doorLock': false,
+    'bonnetLock': true,
+    'trunkLock': true
+}
+
+// Toyota Lock Device uses HTTP as default strategy
+const toyotaLockDevice = new IoTDevice(new HttpCommunicationStrategy());
+
+// Sending data in HTTP Protocol
+toyotaLockDevice.sendData(payload);
+
+// Toyota Lock Device changing strategy into MQTT
+toyotaLockDevice.setCommunicationStrategy(new MqttCommunicationStrategy());
+
+// Sending data in MQTT Protocol
+toyotaLockDevice.sendData(payload);
+
+// Honda Lock Device uses WSS as default strategy
+const hondaLockDevice = new IoTDevice(new WssCommunicationStrategy());
+
+// Sending data in WSS Protocol
+hondaLockDevice.sendData(payload);
+
+
