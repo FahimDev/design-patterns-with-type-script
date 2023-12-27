8eefa5c00002b28865f23cbe65667914b09d980f
Md.Ariful Islam Fahim <43854345+FahimDev@users.noreply.github.com>
2023-12-27 19:23:06 +0600

    feat: Strategy - Implements MQTT Protocol as Strategy

--- strategy/strategy.ts
+++ strategy/strategy.ts
@@ -1,4 +1,13 @@
 // Define a strategy interface for communication
 interface CommunicationStrategy {
-    send(data: string): void;
+  send(data: string): void;
+}
+
+// Concrete implementation of a communication strategy using MQTT protocol
+class MqttCommunicationStrategy implements CommunicationStrategy {
+  send(data: string): void {
+    console.log(`Sending data via MQTT: ${data}`);
   }
+}
+
+
